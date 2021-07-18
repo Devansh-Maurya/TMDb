96b7355c38200b3603ce3a3365b2f4c748eba425
Devansh Maurya <Devansh233@gmail.com>
2021-07-18 16:46:12 +0530

    Inject HomeListFragment

--- app/src/main/java/maurya/devansh/tmdb/di/module/BindingFragmentModule.java
+++ app/src/main/java/maurya/devansh/tmdb/di/module/BindingFragmentModule.java
@@ -4,6 +4,7 @@ import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import maurya.devansh.tmdb.di.scope.PerFragment;
 import maurya.devansh.tmdb.ui.home.HomeFragment;
+import maurya.devansh.tmdb.ui.home.HomeListFragment;
 
 /**
  * Created by devansh on 18/07/21.
@@ -15,4 +16,8 @@ public abstract class BindingFragmentModule {
     @ContributesAndroidInjector(modules = {FragmentModule.class})
     @PerFragment
     public abstract HomeFragment contributeHomeFragment();
+
+    @ContributesAndroidInjector(modules = {FragmentModule.class})
+    @PerFragment
+    public abstract HomeListFragment contributeHomeListFragment();
 }
