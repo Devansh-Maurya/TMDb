9784c3c3f66dce299c7d4260e1ebb949355f2a82
Devansh Maurya <Devansh233@gmail.com>
2021-07-21 16:49:36 +0530

    Add SearchView UI

--- app/src/main/java/maurya/devansh/tmdb/ui/search/SearchFragment.java
+++ app/src/main/java/maurya/devansh/tmdb/ui/search/SearchFragment.java
@@ -6,11 +6,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.util.Pair;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 import org.jetbrains.annotations.NotNull;
 
+import maurya.devansh.tmdb.R;
 import maurya.devansh.tmdb.databinding.FragmentSearchBinding;
 import maurya.devansh.tmdb.ui.base.DaggerBaseFragment;
 
@@ -28,5 +30,31 @@ public class SearchFragment extends DaggerBaseFragment<SearchViewModel, Fragment
 
     @Override
     protected void setupView(@NonNull @NotNull View view) {
+        setHasOptionsMenu(true);
+        binding().toolbar.inflateMenu(R.menu.menu_search);
+        binding().toolbar.setOnMenuItemClickListener(menuItem -> {
+            final SearchView searchView = (SearchView) menuItem.getActionView();
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // perform query here
+                    toast(query);
+
+                    // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                    // see https://code.google.com/p/android/issues/detail?id=24599
+                    searchView.clearFocus();
+
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    toast(newText);
+                    return false;
+                }
+            });
+
+            return true;
+        });
     }
 }
--- app/src/main/java/maurya/devansh/tmdb/ui/search/SearchViewModel.java
+++ app/src/main/java/maurya/devansh/tmdb/ui/search/SearchViewModel.java
@@ -6,11 +6,9 @@ import androidx.paging.PagingData;
 import javax.inject.Inject;
 
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
 import maurya.devansh.tmdb.data.model.Movie;
 import maurya.devansh.tmdb.data.repository.MovieRepository;
 import maurya.devansh.tmdb.ui.base.BaseViewModel;
-import maurya.devansh.tmdb.utils.common.MoviesListType;
 
 /**
  * Created by devansh on 21/07/21.
@@ -19,8 +17,6 @@ import maurya.devansh.tmdb.utils.common.MoviesListType;
 public class SearchViewModel extends BaseViewModel {
 
     private final MovieRepository movieRepository;
-
-    public LiveData<PagingData<Movie>> movieListLiveData = null;
 
     @Inject
     SearchViewModel(
@@ -31,20 +27,7 @@ public class SearchViewModel extends BaseViewModel {
         this.movieRepository = movieRepository;
     }
 
-    public LiveData<PagingData<Movie>> getMovies(@MoviesListType int movieListType) {
-        if (movieListLiveData == null) {
-            movieListLiveData = movieRepository.getMoviesList(movieListType);
-        }
-        return movieListLiveData;
+    public LiveData<PagingData<Movie>> searchMovies(String query) {
+        return movieRepository.searchMovies(query);
     }
-
-    public void bookmarkMovie(Movie movie, boolean isBookmarked) {
-        compositeDisposable.add(movieRepository.bookmarkMovie(movie, isBookmarked)
-            .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.io())
-            .subscribe()
-        );
-    }
-
-//    public void searchMovies(String query)
 }
