3bb053e7e2386ab89bdeb9931e20785b7277559f
Devansh Maurya <Devansh233@gmail.com>
2021-07-23 00:50:09 +0530

    Add progress bar in DetailFragment

--- app/src/main/java/maurya/devansh/tmdb/ui/detail/DetailFragment.java
+++ app/src/main/java/maurya/devansh/tmdb/ui/detail/DetailFragment.java
@@ -14,6 +14,8 @@ import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
 
 import maurya.devansh.tmdb.R;
 import maurya.devansh.tmdb.data.model.Movie;
@@ -59,11 +61,14 @@ public class DetailFragment extends DaggerBaseFragment<DetailViewModel, Fragment
     }
 
     private void updateUi(MovieDetail data) {
+        binding().progressBar.setVisibility(View.GONE);
         binding().groupLayout.setVisibility(View.VISIBLE);
 
         String posterUrl = ApiUtils.getTmdbImageUrl(data.posterPath);
         Glide.with(binding().ivPoster)
             .load(posterUrl)
+            .transition(DrawableTransitionOptions.withCrossFade(200))
+            .apply(new RequestOptions().error(R.drawable.ic_movie_filled))
             .into(binding().ivPoster);
         binding().tvTitle.setText(data.title);
         binding().tvInfo.setText(data.getInfoString());
