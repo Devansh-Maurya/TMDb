ddd0e049a606ef059d073012a41c08a90a5e881f
Devansh Maurya <Devansh233@gmail.com>
2021-07-22 22:01:48 +0530

    Add transition animations

--- app/src/main/java/maurya/devansh/tmdb/ui/MainActivity.java
+++ app/src/main/java/maurya/devansh/tmdb/ui/MainActivity.java
@@ -2,6 +2,8 @@ package maurya.devansh.tmdb.ui;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -9,6 +11,7 @@ import androidx.annotation.AttrRes;
 import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.core.util.Pair;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelStoreOwner;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -40,8 +43,12 @@ public class MainActivity extends DaggerBaseActivity<MainViewModel, ActivityMain
                 getWindow().setStatusBarColor(getColorFromAttribute(android.R.attr.colorBackground));
                 binding.bottomNavigation.setVisibility(View.GONE);
             } else {
-                getWindow().setStatusBarColor(getColorFromAttribute(R.attr.colorPrimaryVariant));
-                binding.bottomNavigation.setVisibility(View.VISIBLE);
+                new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                    if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+                        getWindow().setStatusBarColor(getColorFromAttribute(R.attr.colorPrimaryVariant));
+                        binding.bottomNavigation.setVisibility(View.VISIBLE);
+                    }
+                }, getResources().getInteger(android.R.integer.config_mediumAnimTime));
             }
         }));
     }
