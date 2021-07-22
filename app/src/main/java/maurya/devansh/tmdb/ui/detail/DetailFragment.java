7fe83f88d09aa896e351a1a29b5a25a876f231c3
Devansh Maurya <Devansh233@gmail.com>
2021-07-22 09:37:18 +0530

    Add share functionality

--- app/src/main/java/maurya/devansh/tmdb/ui/detail/DetailFragment.java
+++ app/src/main/java/maurya/devansh/tmdb/ui/detail/DetailFragment.java
@@ -1,11 +1,13 @@
 package maurya.devansh.tmdb.ui.detail;
 
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ShareCompat;
 import androidx.core.util.Pair;
 import androidx.lifecycle.ViewModelStoreOwner;
 
@@ -56,6 +58,8 @@ public class DetailFragment extends DaggerBaseFragment<DetailViewModel, Fragment
         binding().tvTitle.setText(data.title);
         binding().tvInfo.setText(data.getInfoString());
         binding().tvOverview.setText(data.overview);
+
+        binding().ivShare.setOnClickListener(v -> shareMovie(data));
     }
 
     @Override
@@ -65,5 +69,19 @@ public class DetailFragment extends DaggerBaseFragment<DetailViewModel, Fragment
 //            viewModel.bookmarkMovie(movieBookmarked.movie, movieBookmarked.isBookmarked);
             toast(movieBookmarked.movie.title + movieBookmarked.isBookmarked);
         }
+    }
+
+    private void shareMovie(MovieDetail data) {
+        viewModel.getShareableViewImageUri(binding().ivPoster).observe(getViewLifecycleOwner(), path -> {
+            String shareText = String.format(
+                "Checkout *%s* and other movies at the TMDb app! Click the link below:\n%s",
+                data.title, "tmdbapp://movie/detail/" + data.id
+            );
+            new ShareCompat.IntentBuilder(binding().getRoot().getContext())
+                .setText(shareText)
+                .setType("image/*")
+                .setStream(Uri.parse(path))
+                .startChooser();
+        });
     }
 }
