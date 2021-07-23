5c2c7ff44d11d3117e82892ef92b2b8e337a18d6
Devansh Maurya <Devansh233@gmail.com>
2021-07-23 09:49:20 +0530

    Add keep annotations to model classes

--- app/src/main/java/maurya/devansh/tmdb/data/model/NowPlayingMovie.java
+++ app/src/main/java/maurya/devansh/tmdb/data/model/NowPlayingMovie.java
@@ -1,11 +1,13 @@
 package maurya.devansh.tmdb.data.model;
 
+import androidx.annotation.Keep;
 import androidx.room.Entity;
 
 /**
  * Created by devansh on 19/07/21.
  */
 
+@Keep
 @Entity(tableName = "now_playing_movie")
 public class NowPlayingMovie extends MovieId {
 
